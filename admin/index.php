db7721d99ae0da10f2fe0b879f67e1c4b789cb34
Chema <neo22s@gmaill.com>
2012-12-08 09:34:23 +0100

    Added new themes, deleted forum rss

--- admin/index.php
+++ admin/index.php
@@ -30,13 +30,29 @@ require_once('header.php');
 	<thead>
 		<tr>
 			<th width="50%"><a href="http://open-classifieds.com/about/blog/" target="_blank"><?php _e("Blog Updates");?></a></th>
-			<th width="50%"><a href="http://open-classifieds.com/forums/" target="_blank"><?php _e("Support Forum");?></a></th>
+			<th width="50%"><a href="http://open-classifieds.com/themes" target="_blank"><?php _e("Themes");?></a></th>
 		</tr>
 	</thead>
 	<tbody>
 		<tr>
 			<td><?php echo '<ul>'.rssReader('http://open-classifieds.com/feed/',10,CACHE_ACTIVE,'<li>','</li>').'</ul>';?></td>
-			<td><?php echo '<ul>'.rssReader('http://open-classifieds.com/forums/forum/support-2/feed/',10,CACHE_ACTIVE,'<li>','</li>').'</ul>';			?></td>
+			<td>
+				<ul>
+	<li><a title="Demo Open Classifieds" href="http://open-classifieds.com/demo/?theme=kamaleon">Kamaleon</a></li>
+	<li><a title="Demo Open Classifieds" href="http://open-classifieds.com/demo/?theme=responsive">Responsive</a></li>
+	<li><a title="Demo Open Classifieds" href="http://open-classifieds.com/demo/?theme=moderndeluxe">Moderndeluxe</a></li>
+	<li><a title="Demo Open Classifieds" href="http://open-classifieds.com/demo/?theme=yenii">yenii</a></li>
+	<li><a title="Demo Open Classifieds" href="http://open-classifieds.com/demo/?theme=wpClassifieds">wpClassifieds</a></li>
+	<li><a title="Demo Open Classifieds hostpel" href="http://open-classifieds.com/demo/?theme=hostpel">hostpel</a></li>
+	<li><a title="Demo Open Classifieds Primitive" href="http://open-classifieds.com/demo/?theme=primitive">Primitive</a></li>
+	<li><a title="Demo Open Classifieds Simply Fluid" href="http://open-classifieds.com/demo/?theme=simplyfluid">Simply Fluid</a></li>
+	<li><a title="Demo Open Classifieds Edit_80" href="http://open-classifieds.com/demo/?theme=edit_80">Edit_80</a></li>
+	<li><a title="Demo Open Classifieds anunciamex" href="http://open-classifieds.com/demo/?theme=anunciamex">anunciamex</a></li>
+	<li><a title="Demo Open Classifieds aqueous" href="http://open-classifieds.com/demo/?theme=aqueous">aqueous</a></li>
+	<li><a title="Demo Open Classifieds aqueous" href="http://open-classifieds.com/demo/?theme=gallery-blue">Gallery Blue</a></li>
+	<li><a href="http://open-classifieds.com/demo/?theme=post-it-board">Post it Board</a></li>
+	</ul>
+			</td>
 		</tr>
 	</tbody>
 </table>
